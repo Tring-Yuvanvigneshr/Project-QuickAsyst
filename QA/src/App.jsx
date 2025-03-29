9e0f0d80196ff89bfcc880178c59c426e30e2dca
Tring-yuvanvignesh <yuvanvignesh.r@tringapps.com>
2025-03-29 11:58:50 +0530

    cognito handled

--- QA/src/App.jsx
+++ QA/src/App.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import SignIN from './pages/signIn/SignIn';
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Amplify } from 'aws-amplify';
+import { awsConfig } from './aws/aws-exports.js';
+
+Amplify.configure(awsConfig);
 
 const App = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='login' element={<SignIN />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <ToastContainer position="top-center" autoClose={3000} />
+      <Routes>
+        <Route path='login' element={<SignIN />}></Route>
+      </Routes>
+    </BrowserRouter>
+
   )
 }
 
